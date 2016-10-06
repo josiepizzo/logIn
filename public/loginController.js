d9b52cef58fbae8a50039441446555014bf5e21c
judith pizzolato <judith pizzolato>
2016-10-06 16:06:55 -0400

    initial commit

---
+++ public/loginController.js
@@ -0,0 +1,30 @@
+    //Create a module and create a controller for angular
+        var myangularapp = angular.module("myangularapp", []);
+myangularapp.controller("myangularController", function($scope,$http) {
+       
+        
+    $scope.login = function (){
+     alert('test3');
+    
+        $scope.username1;
+        $scope.password1;
+        
+        $http.post('/login', { username: $scope.username1,
+       password: $scope.password1,
+             }).then(function(response) {
+        alert('hi i am working');
+        //alert(JSON.stringify(response));
+        alert(response.data[0].status);
+            if(response.data[0].status == 1){
+                //window.location.href = 'http://joelwebsites.com';
+                console.log("Login Successful");
+            }
+            
+    }, function errorCallback(response) {
+            alert('error');
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+    }
+    
+});
